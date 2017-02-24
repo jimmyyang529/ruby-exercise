6b69c6054cdd117b6a6d42d60726ed939879411b
jimmy <jimmy@jimmyde-MacBook-Pro.local>
2017-02-24 12:13:46 +0800

    第6題開始貼上題目說明

---
+++ homework/6majority_element/6majorityelement.rb
@@ -0,0 +1,22 @@
+#6.Majority Element
+#https://leetcode.com/problems/majority-element/?tab=Description
+#Skills you might need: [Array#sort] [Array#length]
+#Description:
+#Given an array of size n, find the majority element. The majority element is the element that appears more than "n/2" times.
+#You may assume that the array is non-empty and the majority element always exist in the array.
+#給定一個含有 n 個元素的陣列，請找出它的多數為哪一個元素。出現超過 n / 2 次的元素即為多數。
+#你可以假設陣列一定不為空陣列，且陣列中一定存在多數。
+
+#Examples:
+#majority_element([0]) should return 0.
+#majority_element([1,1,2,1,3]) should return 1.
+# @param {Integer[]} nums
+# @return {Integer}
+majority_element([1,2, 2, 3, 3, 3, 3]) # => 3
+
+def majority_element(n)
+
+    
+end
+
+
